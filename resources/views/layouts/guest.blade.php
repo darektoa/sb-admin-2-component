417703fd09e44cc7fdee01dbd3d7ad9a20f6d432
darektoa <darektoa@gmail.com>
2022-01-26 05:08:22 +0700

    Add: Guest layouts

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>
+      @hasSection('title') @yield('title') @else Guest Route @endif
+    </title>
+    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="author" content="Abdul">
+    <meta name="description" content="Type your web description">
+    <meta name="theme-color" content="#FFF">
+    <meta name="title" content="Guest Route">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @yield('head')
+  </head>
+  <body>
+    @yield('content')
+
+    @yield('scripts')
+    <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+  </body>
+</html>
