de02f6e2e73c4c8d5af7fa1aa4d253cc2783507d
darektoa <darektoa@gmail.com>
2022-01-26 09:42:53 +0700

    Add: Sidebar blade component

---
+++ resources/views/components/sidebar/index.blade.php
@@ -0,0 +1,32 @@
+@php
+    $transactionNav = [
+        'Topup'   => url('/transactions') . '?type=1', 
+        'Buying'  => url('/transactions') . '?type=2',
+        'Refund'  => url('/transactions') . '?type=3',
+    ];
+@endphp
+
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
+    <x-sidebar.brand 
+      img="{{ asset('assets/img/brand.png') }}"
+      name="REXENSOFT"
+      route="/dashboard" />
+  
+    <x-sidebar.divider />
+  
+    <x-sidebar.nav-item
+      active="{{ Request::is('dashboard') }}"
+      icon="fa-tachometer-alt"
+      name="Dashboard" 
+      route="{{ url('/dashboard') }}" />
+  
+    <x-sidebar.nav-collapse-item
+      active="{{ Request::is('coins') }}"
+      icon="fa-clipboard-list"
+      name="Transactions"
+      routes="{{ json_encode($transactionNav) }}" />  
+  
+    <x-sidebar.divider mb="4"/>
+    
+    <x-sidebar.toggle/> 
+  </ul>
