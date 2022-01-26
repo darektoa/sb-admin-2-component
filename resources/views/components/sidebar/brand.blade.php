fd16810aa7bcad193367942b37c3f5a77189b8c3
darektoa <darektoa@gmail.com>
2022-01-26 08:18:58 +0700

    Add: Width and height attribute at image in brand blade component

--- resources/views/components/sidebar/brand.blade.php
+++ resources/views/components/sidebar/brand.blade.php
@@ -1,6 +1,6 @@
 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ $route }}">
     <div class="sidebar-brand-icon">
-        <img src="{{ $img }}" alt=" ">
+        <img src="{{ $img }}" width="48" height="48" alt=" ">
     </div>
     <div class="sidebar-brand-text mx-3">{{ $name }}</div>
 </a>
