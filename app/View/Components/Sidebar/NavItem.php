8e57b660bb4efa9762fec5d0a29bda926fbd93fb
darektoa <darektoa@gmail.com>
2022-01-26 07:34:18 +0700

    Add: Sidebar nav item blade component

---
+++ app/View/Components/Sidebar/NavItem.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components\Sidebar;
+
+use Illuminate\View\Component;
+
+class NavItem extends Component
+{
+    public $active;
+    public $icon;
+    public $name;
+    public $route;
+
+
+    public function __construct($active, $icon, $name, $route)
+    {
+        $this->active   = $this->activeClassName($active);
+        $this->icon     = $icon;
+        $this->name     = $name;
+        $this->route    = $route;
+    }
+
+
+    public function render()
+    {
+        return view('components.sidebar.nav-item');
+    }
+
+
+    public function activeClassName($active)
+    {
+        return $this->active = $active ? 'active' : '';
+    }
+}
---
+++ resources/views/components/sidebar/nav-item.blade.php
@@ -0,0 +1,6 @@
+<li class="nav-item {{ $active }}">
+    <a class="nav-link" href="{{ $route }}">
+        <i class="fas fa-fw {{ $icon }}"></i>
+        <span>{{ __($name) }}</span>
+    </a>
+</li>
