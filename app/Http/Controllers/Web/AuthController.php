afd19a29637bffdbd02e6c5944847b4ba7a75066
darektoa <darektoa@gmail.com>
2022-01-27 15:04:33 +0700

    Add: Login method in auth web controller

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -4,10 +4,29 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function loginView() {
         return view('pages.guest.login');
+    }
+
+
+    public function login(Request $request) {
+        $email      = $request->email;
+        $password   = $request->password;
+
+        $account = Auth::attempt([
+            'email'     => $email,
+            'password'  => $password,
+        ]);
+
+        if(!$account) 
+            return back()
+            ->withErrors(["Account doesn't match"])
+            ->withInput();
+
+        return view('layouts.app');
     }
 }
