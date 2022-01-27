36d3145cc5796ed03e0c1faba3d17c7a854803c0
darektoa <darektoa@gmail.com>
2022-01-27 14:45:01 +0700

    Add: Link favicon in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}">
+    <link rel="icon" href="{{ asset('assets/img/brand.png') }}">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="author" content="Abdul">
