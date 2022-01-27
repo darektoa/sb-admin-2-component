ee057b0218c0678a51dfd88678fed29ea40a8b24
darektoa <darektoa@gmail.com>
2022-01-27 14:53:19 +0700

    Add: Hash at password property value in user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,20 +4,16 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         User::create([
             'name'      => 'User',
-            'email'     => 'user@email.com',
-            'password'  => 'password'
+            'email'     => 'user@gmail.com',
+            'password'  => Hash::make('password'),
         ]);
     }
 }
