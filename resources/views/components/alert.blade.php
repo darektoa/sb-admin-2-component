577fd5de1199fca6f0b965778d559cf26dc613de
darektoa <darektoa@gmail.com>
2022-01-27 12:06:34 +0700

    Edit: Delete border left class in alert blade component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-<div class="alert border-left-danger {{ $attributes['class'] }}" role="alert">
+<div class="alert {{ $attributes['class'] }}" role="alert">
     <ul class="pl-4 my-2">
         @foreach ($value->all() as $item)
             <li>{{ $item }}</li>
