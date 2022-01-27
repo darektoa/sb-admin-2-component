380920d65077700fd84d5ad59312f465d23311ca
darektoa <darektoa@gmail.com>
2022-01-27 15:08:43 +0700

    Add: Login web route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ namespace App\Http\Controllers\Web;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginView');
+Route::post('/login', [AuthController::class, 'login'])->name('login'); 
