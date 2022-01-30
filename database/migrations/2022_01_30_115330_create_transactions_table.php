7721fe535432d202ee3027b7665d7501858d6b00
darektoa <darektoa@gmail.com>
2022-01-30 18:56:54 +0700

    Add: Transactions table migration

---
+++ database/migrations/2022_01_30_115330_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('receiver_id')->constrained('users');
+            $table->string('code', 16)->unique();
+            $table->bigInteger('amount');
+            $table->smallInteger('status')->default(1);
+            $table->smallInteger('type');
+            $table->json('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
