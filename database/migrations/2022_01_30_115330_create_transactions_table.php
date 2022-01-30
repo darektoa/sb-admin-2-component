a643d9fc62cb30d64bbe082918e951118ae43f22
darektoa <darektoa@gmail.com>
2022-01-30 19:06:36 +0700

    Add: Nullable at sender_id and detail field in transactions table migration

--- database/migrations/2022_01_30_115330_create_transactions_table.php
+++ database/migrations/2022_01_30_115330_create_transactions_table.php
@@ -15,13 +15,13 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('sender_id')->nullable()->constrained('users');
             $table->foreignId('receiver_id')->constrained('users');
             $table->string('code', 16)->unique();
             $table->bigInteger('amount');
             $table->smallInteger('status')->default(1);
             $table->smallInteger('type');
-            $table->json('detail');
+            $table->json('detail')->nullable();
             $table->timestamps();
         });
     }
