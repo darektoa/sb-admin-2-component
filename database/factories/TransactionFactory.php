969a501d05f9f6c362d4c0abdd9a1e1315adacd5
darektoa <darektoa@gmail.com>
2022-01-31 09:27:58 +0700

    Add: Transaction factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -2,14 +2,25 @@
 
 namespace Database\Factories;
 
+use App\Helpers\RandomHelper;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TransactionFactory extends Factory
 {
     public function definition()
     {
+        $amount     = rand(1, 40) * 500;
+        $code       = strtoupper(RandomHelper::code());
+        $sender     = User::inRandomOrder()->first();
+        $receiver   = User::inRandomOrder()->first();
+
         return [
-            //
+            'sender_id'     => $sender->id,
+            'receiver_id'   => $receiver->id,
+            'code'          => "INV$code",
+            'amount'        => $amount,
+            'type'          => 1,
         ];
     }
 }
