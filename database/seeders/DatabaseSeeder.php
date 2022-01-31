120a1d389c983186f6cc74e4e0ba3be6e39d3fa9
darektoa <darektoa@gmail.com>
2022-01-31 09:59:04 +0700

    Add: Create 20 fake user with transactions in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         $this->call([
             UserSeeder::class,
         ]);
 
-        // \App\Models\User::factory(10)->create();
+        User::factory(20)
+            ->hasSendTransactions(1)
+            ->hasReceiveTransactions(1)
+            ->create();
     }
 }
