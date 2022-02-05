a5c8c013fea81d682359ce4b502fbf7fc1bd14ca
darektoa <darektoa@gmail.com>
2022-02-06 06:38:31 +0700

    Add: Transaction list route in web routes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginView');
 Route::post('/login', [AuthController::class, 'login'])->name('login'); 
+
+Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
