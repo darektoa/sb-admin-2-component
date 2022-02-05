58d91793dbbb9c7dd8ba8b715e46f77832c0fcf0
darektoa <darektoa@gmail.com>
2022-02-06 06:37:29 +0700

    Add: Index method in transaction web controller

--- app/Http/Controllers/Web/TransactionController.php
+++ app/Http/Controllers/Web/TransactionController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
+    public function index() {
+        $transactions = Transaction::latest()
+            ->paginate(10);
+
+        return view('pages.admin.transaction.index', compact('transactions'));
+    }
 }
