4b9d6c85177122917c34a53ffb37c6bfe0032225
darektoa <darektoa@gmail.com>
2022-02-06 12:14:33 +0700

    Add: Bold attribute in text blade component

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -3,14 +3,17 @@
     $inline = false;
     $value  = $attributes['value'];
     $class  = $attributes['class'];
+    $bold   = $attributes['bold'];
 
     if($attributes['href']) $tag = 'a';
     if($attributes['inline']) $inline = true;
     if(!$inline) $attributes['class'] = "d-block $class";
+    if($bold) $attributes['class'] .= ' font-weight-bold';
 
     unset(
         $attributes['inline'],
         $attributes['value'],
+        $attributes['bold'],
     );
 @endphp
 
