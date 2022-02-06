2592d4464bf24025b23844654d1e2ba831a33465
darektoa <darektoa@gmail.com>
2022-02-06 19:31:36 +0700

    Add: Color attribute in submit button blade component

--- resources/views/components/button/submit.blade.php
+++ resources/views/components/button/submit.blade.php
@@ -1,5 +1,18 @@
+@php
+    $class  = $attributes['class'];
+    $color  = $attributes['color'] ?? 'primary';
+    $value  = $attributes['value'];
+    
+    if($color) $attributes['class'] = "btn btn-$color";
+
+    unset(
+        $attributes['color'],
+        $attributes['value'],
+    );
+@endphp
+
 <div class="form-group">
-    <button type="submit" class="btn {{ $attributes['class'] }}" {{ $attributes }}>
-        {{ $attributes['value'] }}
+    <button type="submit" {{ $attributes }}>
+        {{ $value }}
     </button>
 </div>
