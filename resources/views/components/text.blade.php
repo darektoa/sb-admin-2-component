6ad1695134e6ef955c18698c5e2c73b2b2459257
darektoa <darektoa@gmail.com>
2022-02-06 12:52:21 +0700

    Add: Strikethrough attribute in text blade component

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -1,20 +1,22 @@
 @php
-    $tag        = 'p';
-    $inline     = false;
-    $value      = $attributes['value'];
-    $class      = $attributes['class'];
-    $italic     = $attributes['italic'];
-    $bold       = $attributes['bold'];
-    $underline  = $attributes['underline'];
-    $decoration = '';
+    $tag            = 'p';
+    $inline         = false;
+    $value          = $attributes['value'];
+    $class          = $attributes['class'];
+    $italic         = $attributes['italic'];
+    $bold           = $attributes['bold'];
+    $underline      = $attributes['underline'];
+    $strikethrough  = $attributes['strikethrough'];
+    $decoration     = '';
 
     if($attributes['href']) $tag            = 'a';
     if($attributes['inline']) $inline       = true;
     if(!$inline) $attributes['class']       = "d-block $class";
     if($italic) $attributes['class']       .= ' font-weight-bold';
     if($bold) $attributes['class']         .= ' font-italic';
-    if($underline) $decoration             .= 'underline';
-    if($decoration) $attributes['style']    = "text-decoration: $decoration";
+    if($underline) $decoration             .= ' underline';
+    if($strikethrough) $decoration         .= ' line-through';
+    if($decoration) $attributes['style']    = "text-decoration:$decoration";
 
     unset(
         $attributes['inline'],
