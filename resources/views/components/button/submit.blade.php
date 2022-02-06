aa9994b79801b146b5d55c887114bab9ea351d91
darektoa <darektoa@gmail.com>
2022-02-06 18:58:58 +0700

    Edit: Variable value to value from attributes array in submit button blade component

--- resources/views/components/button/submit.blade.php
+++ resources/views/components/button/submit.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
     <button type="submit" class="btn {{ $attributes['class'] }}" {{ $attributes }}>
-        {{ $value }}
+        {{ $attributes['value'] }}
     </button>
 </div>
