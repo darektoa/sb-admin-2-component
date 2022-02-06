ebedfe31efbef205e55b5ed21ea2197d292191a3
darektoa <darektoa@gmail.com>
2022-02-06 12:38:51 +0700

    Add: Italic attribute in text blade component

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -3,16 +3,19 @@
     $inline = false;
     $value  = $attributes['value'];
     $class  = $attributes['class'];
+    $italic = $attributes['italic'];
     $bold   = $attributes['bold'];
 
-    if($attributes['href']) $tag = 'a';
-    if($attributes['inline']) $inline = true;
-    if(!$inline) $attributes['class'] = "d-block $class";
-    if($bold) $attributes['class'] .= ' font-weight-bold';
+    if($attributes['href']) $tag        = 'a';
+    if($attributes['inline']) $inline   = true;
+    if(!$inline) $attributes['class']   = "d-block $class";
+    if($italic) $attributes['class']   .= ' font-weight-bold';
+    if($bold) $attributes['class']     .= ' font-italic';
 
     unset(
         $attributes['inline'],
         $attributes['value'],
+        $attributes['italic'],
         $attributes['bold'],
     );
 @endphp
