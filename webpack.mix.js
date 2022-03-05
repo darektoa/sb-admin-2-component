a0358c18b69721675748e8650bb8111bf7401ba3
darektoa <darektoa@gmail.com>
2022-03-05 08:45:36 +0700

    Add: Combine and minify css files in webpack mix configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.copy('resources/assets/css/app.min.css', 'public/assets/css')
-    .copy('resources/assets/js/scripts', 'public/assets/js/scripts')
-    .copy('resources/assets/js/app.min.js', 'public/assets/js')
+mix.copy('resources/assets/js/scripts', 'public/assets/js/scripts')
     .copy('resources/assets/vendor', 'public/assets/vendor')
     .copy('resources/assets/img', 'public/assets/img')
     .js('resources/assets/js/app.js', 'public/assets/js')
     .sass('resources/assets/sass/app.scss', 'public/assets/css')
-    .postCss('resources/assets/css/app.css', 'public/assets/css', [
-        //
-    ]);
+    .postCss('resources/assets/css/app.css', 'public/assets/css', [])
+    .combine([
+        'resources/assets/css/bootstrap.css',
+        'resources/assets/css/sb-admin-2.css',
+        'resources/assets/css/app.css',
+    ], 'public/assets/css/app.css')
+    .minify('public/assets/css/app.css');
