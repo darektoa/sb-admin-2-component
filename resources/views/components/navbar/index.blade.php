16cfb3ff3fde825956841559efe88eef53dd7f07
darektoa <darektoa@gmail.com>
2022-03-05 09:15:22 +0700

    Edit: Adjust the bootstrap class according to bootstrap five in navbar blade component

--- resources/views/components/navbar/index.blade.php
+++ resources/views/components/navbar/index.blade.php
@@ -4,30 +4,30 @@
   $initial    = $name[0];
 @endphp
 
-<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow-sm">
+  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle me-3">
       <i class="fa fa-bars"></i>
   </button>
 
-  <ul class="navbar-nav ml-auto">
+  <ul class="navbar-nav ms-auto">
     <div class="topbar-divider d-none d-sm-block"></div>
 
     <li class="nav-item dropdown no-arrow">
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ $name }}</span>
+          <span class="me-2 d-none d-lg-inline text-gray-600 small">{{ $name }}</span>
           <figure class="img-profile rounded-circle avatar font-weight-bold" data-initial="{{ $initial }}"></figure>
       </a>
 
       <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
 
           <a class="dropdown-item" href="{{ url('/profile') }}">
-              <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+              <i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>
               Profile
           </a>
 
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="{{ url('/logout') }}">
-              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              <i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>
               Logout
           </a>
       </div>
