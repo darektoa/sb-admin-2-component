99c1b3bd6a09b0565ce98ce9f17a9ed9a05cad0a
darektoa <darektoa@gmail.com>
2022-03-05 09:25:20 +0700

    Fix: Navigation items doesn't appear in nav collapse item blade component

--- resources/views/components/sidebar/nav-collapse-item.blade.php
+++ resources/views/components/sidebar/nav-collapse-item.blade.php
@@ -1,11 +1,11 @@
 <li class="nav-item {{ $active }}">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities{{ Str::replace(' ', '', $name) }}"
+    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUtilities{{ Str::replace(' ', '', $name) }}"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw {{ $icon }}"></i>
         <span>{{ $name }}</span>
     </a>
     <div id="collapseUtilities{{ Str::replace(' ', '', $name) }}" class="collapse" aria-labelledby="headingUtilities"
-        data-parent="#accordionSidebar">
+        data-bs-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
 
             @foreach ($routes as $key => $route)
