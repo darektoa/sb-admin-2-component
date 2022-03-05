a29602fddb89b323e831fda0e48cc82d3c9f576b
darektoa <darektoa@gmail.com>
2022-03-05 09:28:58 +0700

    Edit: Adjust the bootstrap class according to bootstrap five in input blade component

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -5,9 +5,7 @@
 @if($type === 'checkbox')
     <x-input.checkbox {{ $attributes }} />
 @else
-    <div class="form-group">
-        <input type="{{ $type }}" 
-            class="form-control {{ $attributes['class'] }}"
-            {{ $attributes }} />
-    </div>
+    <input type="{{ $type }}" 
+        class="form-control {{ $attributes['class'] }}"
+        {{ $attributes }} />
 @endif
