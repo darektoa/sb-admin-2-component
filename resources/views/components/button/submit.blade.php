b16cdea670b8e348d90ad87f98cd2b8197001a3b
darektoa <darektoa@gmail.com>
2022-03-05 09:32:44 +0700

    Fix: Block style in submit button blade component

--- resources/views/components/button/submit.blade.php
+++ resources/views/components/button/submit.blade.php
@@ -1,13 +1,18 @@
 @php
+    use Illuminate\View\ComponentAttributeBag;
+
     $class      = $attributes['class'];
+    $block      = $attributes['block'];
     $color      = $attributes['color'] ?? 'primary';
     $value      = $attributes['value'];
     $outline    = $attributes['outline'];
+    $wrapper    = new ComponentAttributeBag();
     
     $attributes['class'] = "btn $class";
 
     if(!$outline) $attributes['class'] .= " btn-$color";
     if($outline) $attributes['class']  .= " btn-outline-$color";
+    if($block) $wrapper['class']        = "d-grid";
 
     unset(
         $attributes['color'],
@@ -16,7 +21,7 @@
     );
 @endphp
 
-<div class="form-group">
+<div {{ $wrapper }}>
     <button type="submit" {{ $attributes }}>
         {{ $value }}
     </button>
