7d4233aeab3a204d2c3d8759558d8f1830633957
darektoa <darektoa@gmail.com>
2022-03-08 03:04:16 +0700

    Add: Dashboard route in web routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginView');
-Route::post('/login', [AuthController::class, 'login'])->name('login'); 
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
+Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
