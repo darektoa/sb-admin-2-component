a5f8e36b5fda205ec275443005ee780bb76b4de2
darektoa <darektoa@gmail.com>
2022-03-08 16:51:01 +0700

    Add: Using route name at dashboard route in web routes

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginView');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
