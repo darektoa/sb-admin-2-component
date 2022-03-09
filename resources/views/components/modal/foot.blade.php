a8a3d937e39c900349a594a34a8daab943eb54a1
darektoa <darektoa@gmail.com>
2022-03-09 08:28:58 +0700

    Add: Main and cross attributes in modal foot blade component

--- resources/views/components/modal/foot.blade.php
+++ resources/views/components/modal/foot.blade.php
@@ -1,3 +1,18 @@
-<div class="modal-footer {{ $attributes['class'] }}" {{ $attributes }}>
+@php
+    $class = $attributes['class'];
+    $cross = $attributes['cross'];
+    $main  = $attributes['main'];
+
+    $attributes['class']             = "modal-footer $class";
+    if($main) $attributes['class']  .= " justify-content-$main";
+    if($cross) $attributes['class'] .= " align-items-$cross";
+    
+    unset(
+        $attributes['main'],
+        $attributes['cross'],
+    );
+@endphp
+
+<div class="{{ $attributes['class'] }}" {{ $attributes }}>
    {{ $slot }}
 </div>
