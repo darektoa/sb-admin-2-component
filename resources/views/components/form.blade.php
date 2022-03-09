edf430909fbc7a235d521d73b1aa4e9521fc022d
darektoa <darektoa@gmail.com>
2022-03-09 08:15:15 +0700

    Fix: Method attribute not supporting other methods in form blade component

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,7 +1,6 @@
 @php
     $method = strtoupper($method);
     $isGet  = $method === 'GET';
-    $method = $isGet ? 'GET' : 'POST';
     $inline = $attributes['inline'];
 
     if($inline) $attributes['class'] .= ' form-inline';
@@ -11,7 +10,7 @@
     );
 @endphp
 
-<form action="{{ $action }}" method="{{ $method }}" enctype="multipart/form-data" {{ $attributes }}>
+<form action="{{ $action }}" method="{{ $isGet ? 'GET' : 'POST' }}" enctype="multipart/form-data" {{ $attributes }}>
     @if(!$isGet) 
         @method($method) 
         @csrf
