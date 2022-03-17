fd5bcffea9e9440883582cc3b367f437a5804bc4
darektoa <darektoa@gmail.com>
2022-03-18 00:08:35 +0700

    Fix: Label checkbox not appear in input index blade component

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -4,16 +4,16 @@
     $label  = $attributes['label'];
 
     $attributes['id'] = $id;
-
-    unset(
-        $attributes['label']
-    );
 @endphp
 
-@if($label) <x-input.label :value="$label" :for="$id"/> @endif
 @if($type === 'checkbox')
     <x-input.checkbox {{ $attributes }} />
 @else
+
+    @if($label) 
+    <x-input.label :value="$label" :for="$id"/>
+    @endif
+
     <input type="{{ $type }}" 
         class="form-control {{ $attributes['class'] }}"
         {{ $attributes }} />
