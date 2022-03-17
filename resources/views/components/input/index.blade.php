dcef146f1931e6f722e8288e2b48c6fcfb138b82
darektoa <darektoa@gmail.com>
2022-03-17 23:49:38 +0700

    Add: Generate random id attribute in input index blade component

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -1,13 +1,16 @@
 @php
+    $id     = $attributes['id'] ?? Str::random();
     $type   = $attributes['type'] ?? 'text';
     $label  = $attributes['label'];
+
+    $attributes['id'] = $id;
 
     unset(
         $attributes['label']
     );
 @endphp
 
-@if($label) <x-input.label :value="$label" /> @endif
+@if($label) <x-input.label :value="$label" :for="$id"/> @endif
 @if($type === 'checkbox')
     <x-input.checkbox {{ $attributes }} />
 @else
