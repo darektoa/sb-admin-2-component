e81a2a374cbe3e567691b5f25ad01e47df8c18c1
darektoa <darektoa@gmail.com>
2022-03-18 00:10:45 +0700

    Add: Protect routes with auth middleware in web routes

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginView');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+
+// WITH AUTHENTICATION
+Route::middleware('auth')->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+});
