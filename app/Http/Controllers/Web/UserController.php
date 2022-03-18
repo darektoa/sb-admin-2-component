780ccac25765ec6b6090a9ab2c631001be588855
darektoa <darektoa@gmail.com>
2022-03-18 16:43:52 +0700

    Add: Store method in user web controller

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class UserController extends Controller
 {
@@ -12,5 +15,27 @@ class UserController extends Controller
         $users = User::paginate(10);
         
         return view('pages.admin.users.index', compact('users'));
+    }
+
+
+    public function store(Request $request) {
+        try{
+            $this->validate($request, [
+                'name'  => 'required|min:2|max:50',
+                'email' => 'required|email|unique:users',
+            ]);
+
+            User::create([
+                'name'      => $request->name,
+                'email'     => $request->email,
+                'password'  => Hash::make('password'),
+            ]);
+
+            Alert::success('Success', 'User added successfully');
+        }catch(Exception $err) {
+            Alert::error('Failed', $err->getMessage());
+        }finally {
+            return back();
+        }
     }
 }
