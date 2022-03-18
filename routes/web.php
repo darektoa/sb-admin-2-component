8742954e178c4b5e789954f63009fa9bfa7a0338
darektoa <darektoa@gmail.com>
2022-03-18 16:16:46 +0700

    Add: Redirect slash route to dashboard in web routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn() => redirect()->route('dashboard'));
 Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginView');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
