713c02be00f1d4bc6d788931469cd1a42ec1d910
darektoa <darektoa@gmail.com>
2022-03-19 13:27:20 +0700

    Add: Delete user route in web routes

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,6 @@ Route::middleware('auth')->group(function() {
     Route::prefix('/users')->name('users.')->group(function() {
         Route::get('/', [UserController::class, 'index'])->name('index');
         Route::post('/', [UserController::class, 'store'])->name('store');
+        Route::delete('/{userId}', [UserController::class, 'destroy'])->name('destroy');
     });
 });
