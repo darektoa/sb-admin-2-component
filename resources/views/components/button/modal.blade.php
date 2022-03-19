59da1c90a2742ab63584f3ee076b9397fe98677b
darektoa <darektoa@gmail.com>
2022-03-19 20:07:26 +0700

    Add: Data attribute in button modal blade component

--- resources/views/components/button/modal.blade.php
+++ resources/views/components/button/modal.blade.php
@@ -1,12 +1,45 @@
 @php
+    use Illuminate\Database\Eloquent\Model;
+
     $target = $attributes['target'];
-    
+    $data   = $attributes['data'] ?? [];
+    $id     = $attributes['id'] ??  "button-".Str::random();
+
+    $attributes['id']   = $id;
+    $attributes['type'] = 'button';
+
+    if($data instanceof Model) $data = $data->toArray();
+
     unset(
         $attributes['target'],
-        $attributes['type']
+        $attributes['type'],
+        $attributes['data'],
     );
 @endphp
 
-<x-button type="button" data-bs-toggle="modal" data-bs-target="#{{ $target }}" {{ $attributes }}>
+<x-button data-bs-toggle="modal" data-bs-target="#{{ $target }}" {{ $attributes }}>
     {{ $value ?? $slot }}
 </x-button>
+
+<x-layout.section scripts>
+    <script>
+        const onClickHandler = (data, target) => {
+            const modal  = document.querySelector(`#${target}`);
+            const dialog = modal.querySelector('.modal-dialog');
+            const inputs = Object.keys(data).map((item) => {
+                const elmnt  = modal.querySelector(`*[name=${item}]`);
+                return [item, elmnt || null];
+            }).filter(item => item[1]);
+
+            inputs.forEach(item => item[1].value = data[item[0]]);
+        }
+    </script>
+</x-layout.section>
+
+<x-layout.push scripts>
+    <script>
+        document.querySelector('#{{ $id }}').addEventListener('click', () => {
+            onClickHandler({{ Js::from($data) }}, '{{ $target }}');
+        });
+    </script>
+</x-layout.push>
