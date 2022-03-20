9e420d59c3c4c1fac0096996994d37b84eb1f70f
darektoa <darektoa@gmail.com>
2022-03-20 13:34:14 +0700

    Add: Using searchable trait in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\Models\Searchable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, Searchable, SoftDeletes;
 
     protected $casts = [
         'email_verified_at' => 'datetime',
